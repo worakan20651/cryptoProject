3387b410431e5f2eb73c934139da30f0bd6f2dec
Natcha Klarjing <64050071@KMITL.AC.TH>
2024-04-06 10:19:46 +0700

    create digitalSignature in file ditalSignature.py

--- digitalSignature.py
+++ digitalSignature.py
@@ -1,10 +1,11 @@
 import cryptoMath
 import random
-import hashlib
+import RWHash
 import math
 
 def hash(msg, prime):
-    hashed_msg = int.from_bytes(hashlib.sha256(msg.encode()).digest(), byteorder='big')
+    # msg -> binary string representation
+    hashed_msg = int.from_bytes(msg.encode(), 'big')
     
     size = int(math.log10(prime)+1)
     
@@ -12,7 +13,7 @@ def hash(msg, prime):
     if len(hashed_msg) > size:
         return hashed_msg[:size]
     elif len(hashed_msg) < size:
-        return hashed_msg + b'\x00' * (size - len(hash_value))
+        return hashed_msg + b'\x00' * (size - len(hashed_msg))
     else:
         return hashed_msg
 
@@ -37,10 +38,32 @@ def signature(msg, pbK):
 
     return (hashed_message,r, s)
 
-def verif(msg, pbK, ):
+def verif(msg, pbK, sign):
     p,g,y = pbK
     
     r, s = msg
     
-    gx = cryptoMath.mod_exp(g,)
-    
+    if r < 1 or r >= p or s < 1 or s >= (p - 1):
+        return False
+
+    w = cryptoMath.mod_inverse(s, p - 1)
+    u1 = (hash(msg, p) * w) % (p - 1)
+    u2 = (r * w) % (p - 1)
+    v = (pow(g, u1, p) * pow(y, u2, p)) % p
+
+    return v == r
+
+
+# # Example usage:
+# # Assume private_key and public_key are generated beforehand
+# private_key = (7919, 11, 4327)  # Example private key (p, g, x)
+# public_key = (7919, 11, 4529)   # Example public key (p, g, y)
+
+# msg = "Hello, World!"  # Example message
+
+# # Sign the message
+# signature = sign(msg, private_key, public_key[0], public_key[1])
+
+# # Verify the signature
+# is_valid = verify(msg, signature, public_key)
+# print("Signature is valid:", is_valid)
